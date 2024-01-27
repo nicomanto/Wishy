dc8a8f988ee17e19b2bee7d0b559cf338cff3192
nicomanto <nicomanto49@gmail.com>
2024-01-27 12:09:40 +0100

    test

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context) (string, error) {
-	return fmt.Sprintf("Hello from Golang Lambda!"), nil
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := map[string]string{"message": "ok"}
+
+	// Convert the response to JSON
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	// Return the API Gateway Proxy Response
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(jsonResponse),
+	}, nil
 }
 
 func main() {

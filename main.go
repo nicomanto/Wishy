9950c982502f32c89baacdeadd279f62c1b80174
nicomanto <nicomanto49@gmail.com>
2024-01-27 14:47:00 +0100

    test mongodb connection

--- main.go
+++ main.go
@@ -2,27 +2,57 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"wishy/controllers"
+	"wishy/mongodb"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	response := map[string]string{"message": "ok"}
+	// Connect to MongoDB
+	client := mongodb.Connect(os.Getenv("MONGODB_URI"))
+	db := client.Database(os.Getenv("DB_NAME"))
+	defer client.Disconnect(ctx)
 
-	// Convert the response to JSON
-	jsonResponse, err := json.Marshal(response)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+	endpoint := request.Path
+	// Execute different logic based on the endpoint
+	switch endpoint {
+	case "/categories":
+		response, err := controllers.GetCategeories(ctx, request, db)
+		if err != nil {
+			errInt, e := strconv.Atoi(err.Error())
+			if e != nil {
+				return events.APIGatewayProxyResponse{}, fmt.Errorf("error: %v - %v", err, e)
+			}
+			return events.APIGatewayProxyResponse{
+				StatusCode: errInt,
+				Headers:    map[string]string{"Content-Type": "application/json"},
+			}, fmt.Errorf("get categories: %v", err)
+		}
+		return *response, nil
+	case "/wishes":
+		response, err := controllers.GetWishes(ctx, request, db)
+		if err != nil {
+			errInt, e := strconv.Atoi(err.Error())
+			if e != nil {
+				return events.APIGatewayProxyResponse{}, fmt.Errorf("error: %v - %v", err, e)
+			}
+			return events.APIGatewayProxyResponse{
+				StatusCode: errInt,
+				Headers:    map[string]string{"Content-Type": "application/json"},
+			}, fmt.Errorf("get wishes: %v", err)
+		}
+		return *response, nil
+	default:
+		return events.APIGatewayProxyResponse{
+			StatusCode: 404,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+		}, fmt.Errorf("not found")
 	}
-
-	// Return the API Gateway Proxy Response
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Headers:    map[string]string{"Content-Type": "application/json"},
-		Body:       string(jsonResponse),
-	}, nil
 }
 
 func main() {

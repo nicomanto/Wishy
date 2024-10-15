7620a5d05a74f9c89ea45d05154a2d6ebb6a0356
nicomanto <nicomanto49@gmail.com>
2024-10-16 00:18:12 +0200

    #12

--- controllers/wishes.go
+++ controllers/wishes.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"strings"
@@ -12,12 +13,33 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func GetWishes(ctx context.Context, request events.APIGatewayProxyRequest, db *mongo.Database) (*events.APIGatewayProxyResponse, error) {
-	wishes := []models.WishByCategory{}
+	// get uid
+	uid, exists := request.QueryStringParameters["uid"]
+	if !exists || uid == "" {
+		logrus.Errorln(errors.New("require uid"))
+		return nil, fmt.Errorf("%d", http.StatusBadRequest)
+	}
+	uidObjectId, err := primitive.ObjectIDFromHex(uid)
+	if err != nil {
+		logrus.Errorln(fmt.Errorf("uid not valid: %v", err))
+		return nil, fmt.Errorf("%d", http.StatusBadRequest)
+	}
+	wishes := []models.UserWishes{}
 	cur, err := db.Collection(models.Wish{}.DBCollectionName()).Aggregate(ctx, []bson.M{
+		{"$match": bson.M{"uid": uidObjectId}},
+		{"$lookup": bson.M{
+			"from":         "users",     // The users collection
+			"localField":   "uid",       // Field in the wishes collection (uid)
+			"foreignField": "_id",       // Field in the users collection (_id)
+			"as":           "user_info", // Field to store the result of the lookup
+		}},
+		// Unwind the user_info array if you expect only one user per wish
+		{"$unwind": "$user_info"},
 		{"$sort": bson.M{"name": 1}},
 		{"$group": bson.M{
 			"_id": "$cat.name",
@@ -25,6 +47,7 @@ func GetWishes(ctx context.Context, request events.APIGatewayProxyRequest, db *m
 				"$push": bson.M{"name": "$name", "link": "$link"},
 			},
 		}},
+		{"$addFields": bson.M{"username": "$user_info.name"}},
 		{"$sort": bson.M{"_id": 1}},
 	})
 	if err != nil {
@@ -38,7 +61,7 @@ func GetWishes(ctx context.Context, request events.APIGatewayProxyRequest, db *m
 
 	// load html page
 	var responseBody strings.Builder
-	err = templates.HtmlTpls[templates.WishListHtmlTemplateType].Execute(&responseBody, wishes)
+	err = templates.HtmlTpls[templates.WishListHtmlTemplateType].Execute(&responseBody, wishes[0])
 	if err != nil {
 		logrus.Errorln(err)
 		return nil, fmt.Errorf("%d", http.StatusInternalServerError)
--- main_test.go
+++ main_test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"context"
 	"os"
 	"strings"
 	"testing"
+	"wishy/controllers"
 	"wishy/models"
 	"wishy/templates"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/stretchr/testify/require"
 )
 
-var mockWishesList []models.WishByCategory
+var mockWishesList models.UserWishes
 
 func TestMain(m *testing.M) {
 	setup()
@@ -22,46 +25,48 @@ func TestMain(m *testing.M) {
 func setup() {
 	// pre load html page
 	templates.InitHtmlTpls()
-	mockWishesList = []models.WishByCategory{
-		{
-			Cat: "cat-1",
-			Wishes: []struct {
-				Name string "json:\"name\" bson:\"name\""
-				Link string "json:\"link\" bson:\"link\""
-			}{
-				{
-					Name: "wish-cat-1-name-1",
-					Link: "wish-cat-1-link-1",
-				},
-				{
-					Name: "wish-cat-1-name-2",
-					Link: "wish-cat-1-link-2",
-				},
-				{
-					Name: "wish-cat-1-name-3",
-					Link: "wish-cat-1-link-3",
-				},
-			},
-		},
-		{
-			Cat: "cat-2",
-			Wishes: []struct {
-				Name string "json:\"name\" bson:\"name\""
-				Link string "json:\"link\" bson:\"link\""
-			}{
-				{
-					Name: "wish-cat-2-name-1",
-					Link: "wish-cat-2-link-1",
-				},
-				{
-					Name: "wish-cat-2-name-2",
-					Link: "wish-cat-2-link-2",
-				},
-				{
-					Name: "wish-cat-2-name-3",
-					Link: "wish-cat-2-link-3",
+	mockWishesList = models.UserWishes{
+		Username: "username",
+		Wishes: []models.WishByCategory{
+			{
+				Cat: "cat-1",
+				Wishes: []struct {
+					Name string "json:\"name\" bson:\"name\""
+					Link string "json:\"link\" bson:\"link\""
+				}{
+					{
+						Name: "wish-cat-1-name-1",
+						Link: "wish-cat-1-link-1",
+					},
+					{
+						Name: "wish-cat-1-name-2",
+						Link: "wish-cat-1-link-2",
+					},
+					{
+						Name: "wish-cat-1-name-3",
+						Link: "wish-cat-1-link-3",
+					},
 				},
 			},
+			{
+				Cat: "cat-2",
+				Wishes: []struct {
+					Name string "json:\"name\" bson:\"name\""
+					Link string "json:\"link\" bson:\"link\""
+				}{
+					{
+						Name: "wish-cat-2-name-1",
+						Link: "wish-cat-2-link-1",
+					},
+					{
+						Name: "wish-cat-2-name-2",
+						Link: "wish-cat-2-link-2",
+					},
+					{
+						Name: "wish-cat-2-name-3",
+						Link: "wish-cat-2-link-3",
+					},
+				}},
 		},
 	}
 }
@@ -72,4 +77,17 @@ func TestRenderWishListHtmlPage(t *testing.T) {
 	var responseBody strings.Builder
 	err := templates.HtmlTpls[templates.WishListHtmlTemplateType].Execute(&responseBody, mockWishesList)
 	r.NoError(err)
+}
+
+func TestGetWishesErrors(t *testing.T) {
+	r := require.New(t)
+	resp, err := controllers.GetWishes(context.Background(), events.APIGatewayProxyRequest{QueryStringParameters: map[string]string{}}, nil)
+	r.Nil(resp)
+	r.Equal("400", err.Error())
+	resp, err = controllers.GetWishes(context.Background(), events.APIGatewayProxyRequest{QueryStringParameters: map[string]string{"uid": ""}}, nil)
+	r.Nil(resp)
+	r.Equal("400", err.Error())
+	resp, err = controllers.GetWishes(context.Background(), events.APIGatewayProxyRequest{QueryStringParameters: map[string]string{"uid": "aasdf"}}, nil)
+	r.Nil(resp)
+	r.Equal("400", err.Error())
 }
